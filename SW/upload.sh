48285e8e771441e598d21ed195618b8200089f52
ghostoverflow256 <46546505+GhostDog98@users.noreply.github.com>
2023-04-24 15:55:15 +1000

    Improved upload.sh
    Added comments, added a shebang line at the top, added a feature to detect if the program has been fully written to the pico.

--- SW/upload.sh
+++ SW/upload.sh
@@ -1,11 +1,17 @@
+#!/bin/bash
+
+# Make our uf2 file
 cd ./build/
 make
 cd ..
+
 name=$(find ./build/ -name "*.uf2")
 echo "found: $name"
-RPI="/media/$USER/RPI-RP2/"
+RPI="/run/media/$USER/RPI-RP2/"
 echo "waiting for RPI..."
 timeout=0
+
+# Wait for the rpi to mount
 while [ ! -d "$RPI" ]; do
     sleep 1
     timeout=$((timeout+1))
@@ -15,4 +21,18 @@ while [ ! -d "$RPI" ]; do
     fi
 done
 echo "found $RPI"
-cp $name /media/$USER/RPI-RP2/
+sleep 1
+cp $name /run/media/$USER/RPI-RP2/
+echo "written file"
+
+# Wait for it to unmount automatically, as this means it's finished writing and is safe to unmount.
+# If we unplugged it before this the program has not fully uploaded and been written, despite cp return a code of 1
+while [ -d "$RPI" ]; do
+    sleep 1
+    timeout=$((timeout+1))
+    if [ $timeout -gt 30 ]; then
+        echo "RPI still mounted after 30 seconds, program may not have uploaded successfully"
+        exit 1
+    fi
+done
+echo "RPI has unmounted, program successfully written"
